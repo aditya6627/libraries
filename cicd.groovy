5a47754214ef2fd93fa2374cc9afb73c021b4214
aditya6627 <164343910+aditya6627@users.noreply.github.com>
2024-04-17 18:05:46 +0530

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -2,17 +2,17 @@ def newGit(repo)
 {
  git "${repo}"
 }
- def newmaven()
-{
-sh 'maven package'
-}
-def newdeploy(ip,appname)
-{
-deploy adapters:[tomcat9(credentialsld: '898c13c4-28fd-4283-82d8-c07f78f8a5e8',
-path: ",url: "${ip}")], contextpath: "${appname}", war: '**/*.war'
-}
-def newTest(jobname)
-{
-echo "${jobname}"
-sh "java-jar/home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
-}
+//  def newmaven()
+// {
+// sh 'maven package'
+// }
+// def newdeploy(ip,appname)
+// {
+// deploy adapters:[tomcat9(credentialsld: '898c13c4-28fd-4283-82d8-c07f78f8a5e8',
+// path: ",url: "${ip}")], contextpath: "${appname}", war: '**/*.war'
+// }
+// def newTest(jobname)
+// {
+// echo "${jobname}"
+// sh "java-jar/home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+// }
